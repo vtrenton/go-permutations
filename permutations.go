674f30734e385d31264f717a8cc7d972981b5895
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-09-23 17:20:57 -0500

    refactored and removed a lot of code overhead. making the program more simple overall
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- permutations.go
+++ permutations.go
@@ -5,48 +5,42 @@ import (
 )
 
 type PermutationGenerator struct {
-	nums    []int
-	current []int
-	result  [][]int
+	nums   []int
+	result [][]int
 }
 
-func (pg *PermutationGenerator) Generate() [][]int {
-	pg.permuteHelper()
-	return pg.result
-}
-
-func (pg *PermutationGenerator) permuteHelper() {
-	if len(pg.current) == len(pg.nums) {
+func (pg *PermutationGenerator) permuteHelper(current []int) {
+	if len(current) == len(pg.nums) {
 		// Make a copy of the current slice, as slices are reference types
-		temp := make([]int, len(pg.current))
-		copy(temp, pg.current)
+		temp := make([]int, len(current))
+		copy(temp, current)
 		pg.result = append(pg.result, temp)
 		return
 	}
 
 	for _, num := range pg.nums {
-		if contains(pg.current, num) {
+		if contains(current, num) {
 			continue
 		}
 		// Choose
-		pg.current = append(pg.current, num)
+		newCurrent := append(current, num)
 		// Explore
-		pg.permuteHelper()
-		// Unchoose (backtrack)
-		pg.current = pg.current[:len(pg.current)-1]
+		pg.permuteHelper(newCurrent)
 	}
 }
 
 func main() {
 	scale := 6
 	var nums []int
+
 	for i := range scale {
 		nums = append(nums, i+1)
 	}
 
 	pg := PermutationGenerator{nums: nums}
-	permutations := pg.Generate()
-	for _, p := range permutations {
+	pg.permuteHelper([]int{}) // pass in an empty slice to be used as the local 'current' tracker
+
+	for _, p := range pg.result {
 		fmt.Println(p)
 	}
 }
