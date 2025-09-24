2ce4601ffa92958a05462399fb66b9a18667ece7
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-09-23 19:24:35 -0500

    replaced contains with slices.Contains which is more idiomatic in go
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- permutations.go
+++ permutations.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 type PermutationGenerator struct {
@@ -9,47 +10,45 @@ type PermutationGenerator struct {
 	result [][]int
 }
 
-func (pg *PermutationGenerator) permuteHelper(current []int) {
+func (pg *PermutationGenerator) populateNums(scale int) {
+	for i := range scale {
+		pg.nums = append(pg.nums, i+1)
+	}
+}
+
+func (pg *PermutationGenerator) generator(current []int) {
 	if len(current) == len(pg.nums) {
 		// Make a copy of the current slice, as slices are reference types
 		temp := make([]int, len(current))
-		copy(temp, current)
+		copy(temp, current) // I hate this pattern but it's needed
 		pg.result = append(pg.result, temp)
 		return
 	}
 
 	for _, num := range pg.nums {
-		if contains(current, num) {
+		if slices.Contains(current, num) {
 			continue
 		}
 		// Choose
 		newCurrent := append(current, num)
 		// Explore
-		pg.permuteHelper(newCurrent)
+		pg.generator(newCurrent)
 	}
 }
 
 func main() {
 	scale := 6
-	var nums []int
+	// create object to track global state
+	var pg PermutationGenerator
 
-	for i := range scale {
-		nums = append(nums, i+1)
-	}
+	// give a list of input numbers
+	pg.populateNums(scale)
 
-	pg := PermutationGenerator{nums: nums}
-	pg.permuteHelper([]int{}) // pass in an empty slice to be used as the local 'current' tracker
+	// generate all permutations in a list of lists
+	pg.generator([]int{}) // pass in an empty slice to be used as the local 'current' tracker
 
+	// print lists within the list
 	for _, p := range pg.result {
 		fmt.Println(p)
 	}
-}
-
-func contains(slice []int, val int) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
 }
